e7150867677b58ad7f398c6215fce3842ad8011f
etozheE1m0 <ipopov902@gmail.com>
2023-01-12 17:24:12 +0300

    ыв

--- game.cpp
+++ game.cpp
@@ -7,14 +7,14 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "PUK");
     window.setFramerateLimit(60);
     sf::Texture bg;
-    if (!bg.loadFromFile("bg.png"))
+    if (!bg.loadFromFile("texture/bg.png"))
         return -1;
 
     sf::Sprite s_bg;
     s_bg.setTexture(bg);
 
     sf::Texture hero;
-    if (!hero.loadFromFile("texture.png"))
+    if (!hero.loadFromFile("texture/hero.png"))
         return -1;
     
     sf::Sprite s_hero;
@@ -35,6 +35,6 @@ int main()
         window.draw(s_hero);
         window.display();
     }
-    sdfsdf
+    
     return 0;
 }
