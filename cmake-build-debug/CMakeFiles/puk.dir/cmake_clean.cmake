f68d2c164ffa49eed34c5e21c1eb36cf3e8d01c0
hren_kakoy_to <shuryck.2003@yandex.ru>
2023-01-14 15:30:09 +0300

    абудабе

---
+++ cmake-build-debug/CMakeFiles/puk.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/puk.dir/game.cpp.obj"
+  "CMakeFiles/puk.dir/game.cpp.obj.d"
+  "libpuk.dll.a"
+  "puk.exe"
+  "puk.exe.manifest"
+  "puk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/puk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
