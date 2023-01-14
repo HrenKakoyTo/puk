1478a7226736f4dbb1b61512511acc05507629bb
etozheE1m0 <ipopov902@gmail.com>
2023-01-14 18:13:56 +0300

    абудабе

--- cmake-build-debug/CMakeFiles/puk.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/puk.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "A:/Clion progects/puk/game.cpp" "CMakeFiles/puk.dir/game.cpp.obj" "gcc" "CMakeFiles/puk.dir/game.cpp.obj.d"
+  "C:/puk/game.cpp" "CMakeFiles/puk.dir/game.cpp.obj" "gcc" "CMakeFiles/puk.dir/game.cpp.obj.d"
   )
 
 # Targets to which this target links.
